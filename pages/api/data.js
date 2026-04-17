4ddd26894ceadff68a8b8e7baf4d0bf813d6186b
Frédéric Vamer <fredericvamer@yahoo.fr>
2026-04-17 17:36:33 +0200

    exfilt

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {// fonction exécutée quand on fais : fetch("/api/data", {...})
+export default async function handler(req, res) {// fonction exécutée quand on fais : fetch("/api/data..
 
   try {
     const { city, latitude, longitude } = req.body;//Données reçues, vient de : JSON.stringify
